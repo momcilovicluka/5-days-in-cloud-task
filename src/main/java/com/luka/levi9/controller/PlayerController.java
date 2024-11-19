9fb862054c5d2537ec3b6c46d446ae092303f359
Luka Momčilović <l.momcilovic61@gmail.com>
2024-11-19 19:29:05 +0100

    feat: add get player endpoint and functionality

--- src/main/java/com/luka/levi9/controller/PlayerController.java
+++ src/main/java/com/luka/levi9/controller/PlayerController.java
@@ -1,5 +1,7 @@
 package com.luka.levi9.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,10 @@ public class PlayerController {
 	@PostMapping("/create")
 	public Player createPlayer(@RequestBody Player player) {
 		return playerService.createPlayer(player);
+	}
+
+	@GetMapping("/{id}")
+	public Player getPlayer(@PathVariable String id) {
+		return playerService.getPlayer(id);
 	}
 }
