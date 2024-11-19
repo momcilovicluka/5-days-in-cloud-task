400d0295ece01d902cd382b51edb001bb5f4834e
Luka Momčilović <l.momcilovic61@gmail.com>
2024-11-19 19:26:19 +0100

    feat: add create player endpoint

--- src/main/java/com/luka/levi9/controller/PlayerController.java
+++ src/main/java/com/luka/levi9/controller/PlayerController.java
@@ -1,8 +1,13 @@
 package com.luka.levi9.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luka.levi9.model.Player;
 import com.luka.levi9.service.PlayerService;
+
 @RestController
 @RequestMapping("/players")
 public class PlayerController {
@@ -11,5 +16,10 @@ public class PlayerController {
 
 	public PlayerController(PlayerService playerService) {
 		this.playerService = playerService;
+	}
+
+	@PostMapping("/create")
+	public Player createPlayer(@RequestBody Player player) {
+		return playerService.createPlayer(player);
 	}
 }
