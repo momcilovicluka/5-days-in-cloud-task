5d28d1a90a24f70badb4ea92bffdf4a87e9996c6
Luka Momčilović <l.momcilovic61@gmail.com>
2024-11-19 19:19:01 +0100

    feat: create team model

---
+++ src/main/java/com/luka/levi9/model/Team.java
@@ -0,0 +1,29 @@
+package com.luka.levi9.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NamedQuery(name = "Team.findAll", query = "SELECT t FROM Team t")
+public class Team {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id", updatable = false, nullable = false)
+	private String id;
+
+	private String teamName;
+
+}
