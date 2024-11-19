cfc8eb840b177f4ac8602b51850afa15e2c28c2f
Luka Momčilović <l.momcilovic61@gmail.com>
2024-11-19 19:17:01 +0100

    feat: create player model

---
+++ src/main/java/com/luka/levi9/model/Player.java
@@ -0,0 +1,31 @@
+package com.luka.levi9.model;
+
+import org.hibernate.annotations.NamedQuery;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "nickname" }))
+@NamedQuery(name = "Player.findAll", query = "SELECT p FROM Player p")
+public class Player {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id", updatable = false, nullable = false)
+	private String id;
+	private String nickname;
+	private int wins;
+	private int losses;
+	private int elo;
+	private int hoursPlayed;
+}
