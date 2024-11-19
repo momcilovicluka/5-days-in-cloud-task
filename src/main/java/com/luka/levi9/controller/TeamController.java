1fd209408d11cba4422e039075ba00401fb03d4e
Luka Momčilović <l.momcilovic61@gmail.com>
2024-11-19 21:42:22 +0100

    feat: add create team endpoint

--- src/main/java/com/luka/levi9/controller/TeamController.java
+++ src/main/java/com/luka/levi9/controller/TeamController.java
@@ -1,8 +1,13 @@
 package com.luka.levi9.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luka.levi9.model.Team;
 import com.luka.levi9.service.TeamService;
+
 @RestController
 @RequestMapping("/teams")
 public class TeamController {
@@ -13,4 +18,8 @@ public class TeamController {
 		this.teamService = teamService;
 	}
 
+	@PostMapping()
+	public Team createTeam(@RequestBody Team team) {
+		return teamService.createTeam(team);
+	}
 }
