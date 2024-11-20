1f941fb0178d65f51ae9957b73bee2e26399e5e9
Luka Momčilović <l.momcilovic61@gmail.com>
2024-11-20 12:26:05 +0100

    feat: add ability to find team by name

--- src/main/java/com/luka/levi9/repository/TeamRepository.java
+++ src/main/java/com/luka/levi9/repository/TeamRepository.java
@@ -1,8 +1,12 @@
 package com.luka.levi9.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luka.levi9.model.Team;
 
 public interface TeamRepository extends JpaRepository<Team, String> {
+
+	Optional<Team> findByTeamName(String teamName);
 }
